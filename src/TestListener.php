af233e8d0738ecc0566c181de010fa8e1addf92e
Sebastian Bergmann <sebastian@phpunit.de>
2017-08-13 08:29:10 +0200

    Add TestListener example

---
+++ src/TestListener.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of the PHPUnit Example Extension.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPUnit\ExampleExtension;
+
+use PHPUnit\Framework\TestListenerInterface;
+use PHPUnit\Framework\TestListenerDefaultImplementation;
+use PHPUnit\Framework\Test;
+use PHPUnit\Framework\TestCase;
+
+class TestListener implements TestListenerInterface
+{
+    use TestListenerDefaultImplementation;
+
+    public function startTest(Test $test)
+    {
+        if (!$test instanceof TestCase) {
+            return;
+        }
+
+        // ...
+    }
+
+    // ...
+}
